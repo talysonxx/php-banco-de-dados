02a93439a27c4e2e381242637d50c1642424fcf7
talysonxx <talysonoficial12@gmail.com>
2025-05-15 17:52:15 -0300

    grrefdge

--- projeto/logar.php
+++ projeto/logar.php
@@ -1,5 +1,13 @@
 <?php 
 
+//isset Retorna true se a variável existe e não é null
+if(isset($_POST['email'], $_POST['senha']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
 
+} else {
+    header("Location: index.php");
+};
+
+$login = addslashes($_POST['email']);
+$senha = addslashes($_POST['senha']);
 
 ?>
